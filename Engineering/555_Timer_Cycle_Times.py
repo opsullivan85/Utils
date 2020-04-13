f1c2015576c55874058db6c7e79e671305fd9cd3
Owen Sullivan <opsullivan85@gmail.com>
2020-04-13 10:40:10 -0400

    Added disclamer and first 2 cycle times

--- Engineering/555_Timer_Cycle_Times.py
+++ Engineering/555_Timer_Cycle_Times.py
@@ -1,4 +1,5 @@
 import math
+from sigfig import round
 
 while(True):
     r1 = eval(input('R1: '))
@@ -6,7 +7,12 @@ while(True):
     c1 = eval(input('C1: '))
     td = r2*c1*math.log(2)
     tc = (r1+r2)*c1*math.log(2)
-    print(f'Td = {td}')
-    print(f'Tc = {tc}')
-    print(300%(td+tc))
+    print(f'Td = {round(td, sigfigs=3)}')
+    print(f'Tc = {round(tc, sigfigs=3)}')
+    print('The following don\'t actually include the initial charge time')
+    print(f'state after 5 minutes: {round(300%(td+tc), sigfigs=3)}')
+    print(round(td, sigfigs=3),
+          round(td + tc, sigfigs=3),
+          round(td + tc + td, sigfigs=3),
+          round(td + tc + td + tc, sigfigs=3))
     print()
